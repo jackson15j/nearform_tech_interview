769b23fb1b0efac4705f79bee8fb62a480f8a3f4
Craig Astill <jackson15j@hotmail.com>
2024-03-18 11:41:43 +0000

    feat: Pull in graphene with example graphql query + unit test.

--- src/demo/main.py
+++ src/demo/main.py
@@ -1,2 +1,15 @@
 """TODO: Nearform Main docstring."""
+import graphene
 
+
+class Query(graphene.ObjectType):
+    """GraphQL demo from: https://graphql.org/code/#python."""
+    hello = graphene.String(name=graphene.String(default_value="World"))
+
+    def resolve_hello(self, info, name):
+        return f"Hello {name}"
+
+
+schema = graphene.Schema(query=Query)
+result = schema.execute("{ hello }")
+print(result.data["hello"])  # "Hello World"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,13 @@
 """TODO"""
+from src.demo.main import Query
+import graphene
 
 
 class TestMain:
     def test_a(self):
         assert True
+
+    def test_graphene_graphql_example(self):
+        schema = graphene.Schema(query=Query)
+        result = schema.execute("{ hello }")
+        assert result.data["hello"] == "Hello World"
